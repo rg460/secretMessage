b94969a0204205fc8f0a3a73510544fb2c68ffbe
ravigordon <rgordz460@outlook.com>
2022-05-11 11:46:38 +0100

    removed the first string with .shift()

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 secretMessage.pop();
+secretMessage.push('coding','is','fun');
+secretMessage[7] = 'right';
+secretMessage.shift();
 console.log(secretMessage);
